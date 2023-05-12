157c764f8755e5b3fc46947305d5bf36dad92327
Shelton Francisco <sheltonfjs@gmail.com>
2023-05-12 21:32:30 +0200

    feat: funcionalidade de criacao de cobta

--- app/src/main/java/dev/isutc/chatapplications/models/User.java
+++ app/src/main/java/dev/isutc/chatapplications/models/User.java
@@ -1,4 +1,41 @@
 package dev.isutc.chatapplications.models;
 
 public class User {
+
+    private String uid;
+    private String username;
+    private String profileUrl;
+
+    public User() {
+    }
+
+    public User(String uid, String username, String profileUrl) {
+        this.uid = uid;
+        this.username = username;
+        this.profileUrl = profileUrl;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
 }
--- app/src/main/java/dev/isutc/chatapplications/ui/fragments/SignUpFragment.java
+++ app/src/main/java/dev/isutc/chatapplications/ui/fragments/SignUpFragment.java
@@ -7,17 +7,26 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 import java.io.IOException;
+import java.util.UUID;
 
 import dev.isutc.chatapplications.databinding.FragmentSignUpBinding;
+import dev.isutc.chatapplications.models.User;
 import dev.isutc.chatapplications.ui.listeners.AuthFragmentsListener;
 
 public class SignUpFragment extends Fragment {
@@ -31,10 +40,56 @@ public class SignUpFragment extends Fragment {
     }
 
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.navigateSignin.setOnClickListener(e -> activityCallback.replaceFragment(new SignFragment()));
+        binding.selectImage.setOnClickListener(e -> selectPhoto());
+        binding.loginBtn.setOnClickListener(e -> createUser());
+    }
+
     private void selectPhoto() {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         startActivityForResult(intent, 0);
+    }
+
+    private void createUser() {
+        String email = binding.emailInput.getText().toString();
+        String name = binding.nameInput.getText().toString();
+        String password = binding.passwordInput.getText().toString();
+
+        if (password.isEmpty() || email.isEmpty() || name.isEmpty()) {
+            Toast.makeText(this.getContext(), "Nome, Senha e Email devem ser fornecidos!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        saveUserOnFirebase();
+                    }
+                }).addOnFailureListener(e -> Toast.makeText(SignUpFragment.this.getContext(), e.getMessage(), Toast.LENGTH_SHORT).show());
+    }
+
+    private void saveUserOnFirebase() {
+        String fileName = UUID.randomUUID().toString();
+        final StorageReference reference = FirebaseStorage.getInstance().getReference("/images/" + fileName);
+        reference.putFile(profileUri)
+                .addOnSuccessListener(taskSnapshot -> reference.getDownloadUrl()
+                        .addOnSuccessListener(uri -> {
+                            String uuid = FirebaseAuth.getInstance().getUid();
+                            String username = binding.nameInput.getText().toString();
+                            String profileUrl = uri.toString();
+
+                            User user = new User(uuid, username, profileUrl);
+
+                            FirebaseFirestore.getInstance().collection("users")
+                                    .add(user)
+                                    .addOnSuccessListener(documentReference -> Log.i("TESTE", documentReference.getId())).addOnFailureListener(e -> Log.i("TESTE", e.getMessage()));
+                        }))
+                .addOnFailureListener(e -> Log.i("TESTE", e.getMessage()));
     }
 
 
@@ -42,16 +97,16 @@ public class SignUpFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == 0) {
+        if (requestCode == 0) {
             profileUri = data.getData();
 
-            Bitmap bitmap = null;
+            Bitmap bitmap;
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(this.getContext().getContentResolver(), profileUri);
                 binding.selectedImage.setImageDrawable(new BitmapDrawable(bitmap));
                 binding.selectImage.setAlpha(0);
-            } catch (IOException e){
-
+            } catch (IOException e) {
+                Log.i("TESTE", e.getMessage());
             }
 
         }
@@ -69,18 +124,12 @@ public class SignUpFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         binding = FragmentSignUpBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
 
-        binding.navigateSignin.setOnClickListener(e -> activityCallback.replaceFragment(new SignFragment()));
-        binding.selectImage.setOnClickListener(e -> selectPhoto());
-    }
 }
