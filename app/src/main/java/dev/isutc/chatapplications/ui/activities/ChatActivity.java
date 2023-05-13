ff6633adbcd5fd6b017db85660d854e98951630f
David-Jr <david0gjunior@gmail.com>
2023-05-14 00:32:39 +0200

    Created ChatActivity and its layout file

---
+++ app/src/main/java/dev/isutc/chatapplications/ui/activities/ChatActivity.java
@@ -0,0 +1,21 @@
+package dev.isutc.chatapplications.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dev.isutc.chatapplications.R;
+import dev.isutc.chatapplications.databinding.ActivityChatBinding;
+
+public class ChatActivity extends AppCompatActivity {
+    private ActivityChatBinding chatBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        chatBinding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(chatBinding.getRoot());
+
+
+    }
+}
